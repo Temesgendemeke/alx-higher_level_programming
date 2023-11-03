7ac9f08225f2ca322361d4d1274fddccafc60a0b
temesgendemeke <tdemeke36@gmail.com>
2023-11-03 03:13:59 -0700

    fed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-add = __import__('add_0').add
 a = 1
 b = 2
-print("{0:d} + {1:d} = {2:d}".format(a, b, add(a,b)))
+add = __import__('add_0').add
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
