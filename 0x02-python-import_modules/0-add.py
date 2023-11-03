dfc630b7a79ad145c3e545b9e71e4c88b59b7762
temesgendemeke <tdemeke36@gmail.com>
2023-11-03 03:26:45 -0700

    checks if the program is main or not

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 a = 1
 b = 2
-add = __import__('add_0').add
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    add = __import__('add_0').add
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
