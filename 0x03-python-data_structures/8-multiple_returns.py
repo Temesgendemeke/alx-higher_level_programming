2ab8a55b977b96429ecde8085a0fe1d966be1ef9
temesgendemeke <tdemeke36@gmail.com>
2023-11-04 12:01:43 -0700

    fixed task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = (len(sentence), sentence[0])
-    return a
+    if not sentence:
+        a = (len(sentence), None)
+        return a
+    else:
+        a = (len(sentence), sentence[0])
+        return a
