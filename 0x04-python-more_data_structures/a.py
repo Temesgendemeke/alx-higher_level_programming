fb47a325a59980188c2c9f871915f79661da3daf
temesgendemeke <tdemeke36@gmail.com>
2023-11-08 10:34:26 -0800

    list

--- 0x04-python-more_data_structures/a.py
+++ 0x04-python-more_data_structures/a.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 a = [1,2,2,34,44]
-print(a)
-for i in range(len(a)):
-    if a[i] == 1:
-        a[i] = 8
 
-b = [a = 2 for i in range(len(a)) if a[i] == 1]
+for i in a:
+    ke = []
+    if i == 1:
+        a.append(8)
+    else:
+        a.append(i)
+
