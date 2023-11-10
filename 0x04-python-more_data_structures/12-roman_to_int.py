f936c823728117a4eacafa64aa8006f261d97437
temesgendemeke <tdemeke36@gmail.com>
2023-11-10 10:52:23 -0800

    fg

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-  if not isinstance(roman_string, str):
-    raise ValueError("roman_string must be a string.")
-  roman_numeral_map = {
-      "I": 1,
-      "V": 5,
-      "X": 10,
-      "L": 50,
-      "C": 100,
-      "D": 500,
-      "M": 1000,
-  }
-  result = 0
- 
-  for i in range(len(roman_string)):
-    current_numeral = roman_string[i]
-    if current_numeral not in roman_numeral_map:
-      raise ValueError(f"Invalid Roman numeral character: {current_numeral}")
-    current_value = roman_numeral_map[current_numeral]
-    if i + 1 < len(roman_string) and roman_numeral_map[roman_string[i + 1]] > current_value:
-      result -= current_value
-    else:
-      result += current_value
+    if not isinstance(roman_string, str):
+        return 0
+    roman_numeral_map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+       }
+    result = 0
 
-  return result
+    for i in range(len(roman_string)):
+        curr = roman_string[i]
+        curr_v = roman_numeral_map[curr]
+        if curr not in roman_numeral_map:
+            raise ValueError(f"Invalid Roman numeral character: {curr}")
+        if i + 1 < len(roman_string) and roman_numeral_map[roman_string[i + 1]] > curr_v:
+            result -= curr_v
+        else:
+            result += curr_v
+    return result
