b714004931ccd75f4433c739e6e7554f65e00247
Temesgen dora <tdemeke36@gmail.com>
2023-11-20 08:22:28 -0800

    task 7: fixed string err

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
     for i in range(len(args) - 1):
         try:
             return fct(args[i], args[i + 1])
-        except (ValueError, ZeroDivisionError) as zero:
+        except ZeroDivisionError as zero:
             print("Exception: {}".format(zero), file=sys.stderr)
             return None
             
