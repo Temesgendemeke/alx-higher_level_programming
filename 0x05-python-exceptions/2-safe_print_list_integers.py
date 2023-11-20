ea43b9d690eafa9a6df9bdcdbd225826bb0e412d
Temesgen dora <tdemeke36@gmail.com>
2023-11-20 06:05:31 -0800

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in my_list[:x]:
+        try:
+            count += 1
+            print("{:d}".format(i), end="")
+        except:
+            count -= 1
+            pass
+    print()
+    return count
+            
+            
