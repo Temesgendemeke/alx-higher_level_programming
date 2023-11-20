508ae325bdcc69c7e783b83b75d8afd38ed985c5
Temesgen dora <tdemeke36@gmail.com>
2023-11-20 07:39:16 -0800

    task 7: fixed string error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as va:
+    except (TypeError, ValueError) as va:
         print("Exception: {}".format(va), file=sys.stderr)
         return False
