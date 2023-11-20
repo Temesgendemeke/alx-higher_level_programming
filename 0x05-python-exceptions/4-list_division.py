986fa2264d5c19e6367872f2ed9d63c81cba3966
Temesgen dora <tdemeke36@gmail.com>
2023-11-20 06:51:44 -0800

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div = []
+    for i in range(list_length):
+        try:
+            div.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            div.append(0)
+            print("division by 0")
+        except TypeError:
+            div.append(0)
+            print("wrong type")
+        except IndexError:
+            div.append(0)
+            print("out of range")
+    return div
