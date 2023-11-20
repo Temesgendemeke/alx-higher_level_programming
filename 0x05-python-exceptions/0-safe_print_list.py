975b8eeab801e0cc07929f534bfef8fbc4e4276c
Temesgen dora <tdemeke36@gmail.com>
2023-11-20 05:26:39 -0800

    task 1 done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        count = 0
-        for i in range(x):
-            count += 1
-            try:
-                print(my_list[i], end="")
-            except IndexError:
-                count -= 1
-                break
+    count = 0
+    for i in range(x):
+        count += 1
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            count -= 1
+            break
         print()
-        return count
-        
+    return count
