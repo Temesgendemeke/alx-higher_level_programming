2c5dbc744645b937abdb4f849ae4af10b5275429
Temesgen dora <tdemeke36@gmail.com>
2023-11-22 08:42:00 -0800

    task 3 fix do

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
     """
     Square Module
@@ -21,7 +24,12 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = size ** 2
 
     def area(self):
-        return self.__size ** 2
+        """returns current square area
+
+        Returns:
+            int: current square area
+        """
+        return self.__size
