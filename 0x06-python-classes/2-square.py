c857d421ca1b0cb34660cb86ba646eaeb956451a
Temesgen dora <tdemeke36@gmail.com>
2023-11-22 08:16:52 -0800

    task 2 fix docc2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,13 +3,11 @@ class Square:
     """A module for square"""
     def __init__(self, size=0):
         """
-        creates private attribute size
-        
         checks if size is integer otherwise raises TypeError
         checks if size is greater than zero otherwise raises ValueError
         """
-        self.__size = size
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
