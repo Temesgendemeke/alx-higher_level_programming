4e54b20d544362896947f81d3d2662f56a9ade05
Temesgen dora <tdemeke36@gmail.com>
2023-11-22 08:20:10 -0800

    task 2 fix docc23

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 class Square:
-    """Represent a square."""
+    """
+Square Module
+
+This module defines the Square class, which represents a square
+with a specified side length. It includes a method to obtain a
+dictionary representation of the square.
+
+Example:
+    mysquare = Square(3)
+    print(mysquare.dict_())  # Output: {'side': 3}
+"""
+    
     def __init__(self, size=0):
         """Initialize a new Square.
         Args:
