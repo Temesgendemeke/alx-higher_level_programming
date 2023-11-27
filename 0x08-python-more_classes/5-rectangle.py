0fa55d61afc5ee2ec7cb942950fa80f2e7441f41
Temesgen dora <tdemeke36@gmail.com>
2023-11-27 14:12:38 -0800

    task 5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""documention for module"""
+
+
+class Rectangle:
+    """_summary_
+    """
+    def __init__(self, width=0, height=0):
+        """_summary_
+
+        Args:
+            width (int, optional): _description_. Defaults to 0.
+            height (int, optional): _description_. Defaults to 0.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height attribute setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """width attribute getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width attribute setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def __str__(self) -> str:
+        """
+        string representation of the rectangle by
+        concatenating rows of '#' characters.
+
+        Returns:
+            str: "#" in respect reactangular shape
+        """
+        if self.width == 0 or self.width == 0:
+            return ""
+        rectangle_str = ''
+        for _ in range(self.height):
+            rectangle_str += '#' * self.width + '\n'
+        return rectangle_str
+
+    def __repr__(self) -> str:
+        """returns represention"""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        return "Bye rectangle..."
+
+    def area(self):
+        """returns areas of reactangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """returns perimeter of reactangle"""
+        if self.width == 0 or self.width == 0:
+            return 0
+        return 2 * (self.height + self.width)
