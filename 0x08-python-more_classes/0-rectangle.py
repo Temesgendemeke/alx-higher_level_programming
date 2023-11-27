f26db8a320ffe39b1056f34376a56699d5eaef4c
Temesgen dora <tdemeke36@gmail.com>
2023-11-27 12:00:22 -0800

    task 0 and task 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""_summary"""
+
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+class Rectangle:
+    """_summary_
+    """
+    def __init__(self, width=0, height=0):
+        """_summary_
+
+        Args:
+            width (int, optional): _description_. Defaults to 0.
+            height (int, optional): _description_. Defaults to 0.
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height attribute setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """width attribute getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width attribute setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
