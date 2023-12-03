3ce06c5166908a249a694c3f6b8a5b8bfa433203
Temesgen dora <tdemeke36@gmail.com>
2023-12-03 00:05:14 -0800

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = malloc(sizeof(hash_table_t));
+    if (table == NULL)
+    {
+        return NULL;
+    }
+    table->size = size;
+
+    return table;
+}
+    
