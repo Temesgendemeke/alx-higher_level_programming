1fd8dbc218e8340b8750a1b18cf811f5f76175b5
Temesgen dora <tdemeke36@gmail.com>
2023-12-04 11:03:46 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#1/usr/bin/python3
+class MyList(list): 
+    def __init__(self):
+        super().__init__()
+        
+        
+    def print_sorted(self):
+        self.sort()
+    
+    
+        
+        
+            
+  
+    
+        
+    
+        
+    
