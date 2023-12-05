47e21e817d71cff985d14a5e9750133b5877ae1b
Temesgen dora <tdemeke36@gmail.com>
2023-12-05 04:47:21 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+def read_file(filename=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as files:
+        print(files.read())
