3a0b0fd8f25c8298b9b81ff7df4853ebd64185c2
Temesgen dora <tdemeke36@gmail.com>
2023-12-05 04:59:58 -0800

    task 0 fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,7 +9,8 @@ def read_file(filename=""):
     file reading test
     >>> read_file("./test/my_file_0.txt")
     We offer a truly innovative approach to education:
-    focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
+    focus on building reliable applications and scalable systems,
+    take on real-world challenges, collaborate with your peers.
 
     A school every software engineer would have dreamt of!
     """
