66c5b7526615e624365d1522284f8be6d18c6fad
Temesgen dora <tdemeke36@gmail.com>
2023-12-05 06:00:35 -0800

    task 3

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that appends a string at the end of a text file"""
+def append_write(filename="", text=""):
+    """
+    >>> append_write("tests//my_first_file.txt", "This School is so cool!\n")
+    24
+    """
+    with open(filename, "a+", encoding="utf-8") as f:
+        return f.write(text)
