8c91d258c1ef51372b4433752edc50d0c3a500ad
Temesgen dora <tdemeke36@gmail.com>
2023-12-06 10:57:18 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
+
+
+class Student:
+    """_summary_"""
+    def __init__(self, first_name, last_name, age):
+        """_summary_
+
+        Args:
+            first_name (_type_): _description_
+            last_name (_type_): _description_
+            age (_type_): _description_
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
