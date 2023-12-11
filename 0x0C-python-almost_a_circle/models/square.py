d59cf12b16114d882d766bf721b5209a84da589a
Temesgen dora <tdemeke36@gmail.com>
2023-12-11 09:26:17 -0800

    task fixed

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """_summary_
 
@@ -17,9 +18,10 @@ class Square(Rectangle):
         self.__size = size
         self.width = size
         self.height = size
-        super().__init__(size, size, x = 0, y = 0, id= None)
+        super().__init__(size, size, x=0, y=0, id=None)
         self.x = x
         self.y = y
+
     @property
     def size(self):
         """_summary_
@@ -28,10 +30,11 @@ class Square(Rectangle):
             _type_: _description_
         """
         return self.__size
+
     @size.setter
     def size(self, value):
         """setter"""
-        self.__size = self.width = self.height= value
+        self.__size = self.width = self.height = value
 
     def __str__(self) -> str:
         """_summary_
@@ -40,22 +43,22 @@ class Square(Rectangle):
             str: _description_
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+
     def update(self, *args, **kwargs):
         """_summary_
         """
         len_of_args = len(args)
         if len_of_args == 0 and args is not None:
-                for key, value in kwargs.items():
-                    if key == "id":
-                        self.id = value
-                    elif key == "size":
-                        self.size = value
-                    elif key == "x":
-                        self.x = value
-                    elif key == "y":
-                       self.y = value
-            
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
         if len_of_args == 1:
             self.id = args[0]
         elif len_of_args == 2:
@@ -64,7 +67,7 @@ class Square(Rectangle):
             self.id = args[2]
         elif len_of_args == 4:
             self.size = args[3]
-        
+
     def to_dictionary(self):
         """_summary_
 
@@ -72,4 +75,3 @@ class Square(Rectangle):
             _type_: _description_
         """
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
-    
