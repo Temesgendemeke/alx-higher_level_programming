c880e47ae251a613e03954593bc6bdbb4c5d2a92
Temesgen dora <tdemeke36@gmail.com>
2024-01-16 12:18:14 -0800

    done: task 14 fix #3

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list number f records with the same score
-SELECT score, COUNT(score) FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
