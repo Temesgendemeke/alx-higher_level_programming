ba66ea31dd868176b4e97e0653989ceec42ef3c4
Temesgen dora <tdemeke36@gmail.com>
2024-01-16 13:21:06 -0800

    done

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 --- top cites temprature during july and augest
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (6,8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7,8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
