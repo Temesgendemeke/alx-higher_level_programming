bbcd5bb1ec5180af34f40a8966838d9ac8b38266
Temesgen dora <tdemeke36@gmail.com>
2024-01-17 12:40:08 -0800

    fixed: task 11

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,3 @@
 -- list all cities 
-USE hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM cities 
-INNER JOIN states ON cities.id=states.state_id ORDER BY cities.id ASC;
+INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
