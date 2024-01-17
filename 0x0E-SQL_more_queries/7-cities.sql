de281140297c8ffe07ff16124d8c546b8cf35d31
Temesgen dora <tdemeke36@gmail.com>
2024-01-17 08:50:45 -0800

    fixed #1 : task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id int int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id int NOT NULL,
     FOREIGN KEY state_id REFERENCES states(id),
-    name VARCHAR(256)
-    name
-)
+    name VARCHAR(256) NOT NULL
+);
