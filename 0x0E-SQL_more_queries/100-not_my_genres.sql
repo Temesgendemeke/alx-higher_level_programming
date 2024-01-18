38699f5fe6442b984325f8febea2c03d5d7c6e30
Temesgen dora <tdemeke36@gmail.com>
2024-01-18 10:15:28 -0800

    fixed #3: task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,9 @@
 -- lists geners that are not linked to the show
 SELECT tv_genres.name FROM tv_genres
 WHERE tv_genres.name NOT IN (SELECT tv_genres.name FROM tv_shows
-JOIN tv_show_genres On tv_show_genres.show_id = tv_genres.id
-JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = "Dexter")
-ORDER BY tv_genres.name ASC;
+ORDER BY tv_genres.name;
 
 
