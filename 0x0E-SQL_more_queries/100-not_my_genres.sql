7b822c37691b75fe134610679c27ed4c3f3fa221
Temesgen dora <tdemeke36@gmail.com>
2024-01-18 10:05:56 -0800

    fixed #4: task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,5 +4,6 @@ WHERE tv_genres.name NOT IN (SELECT tv_genres.name FROM tv_shows
 JOIN tv_show_genres On tv_show_genres.show_id = tv_genres.id
 JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = "Dexter")
+ORDER BY tv_genres.name ASC;
 
 
