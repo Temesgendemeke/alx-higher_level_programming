d6899787e3184956db595fec3d02f59f0553851f
Temesgen dora <tdemeke36@gmail.com>
2024-02-12 09:57:47 -0800

    task: 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,8 @@
 #!/usr/bin/node
 const arr = process.argv;
-let first = arr[2];
-let second = first;
 
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  for (let i = 2; i < arr.length; i++) {
-    for (let j = 2; j < arr.length; j++) {
-      if (first < arr[i + 1]) {
-        second = first;
-        first = arr[i + 1];
-      }
-    }
-  }
-  console.log(second);
+if (arr.length <= 3) console.log(0);
+else {
+  const list = arr.sort();
+  console.log(list.reverse()[1]);
 }
