2b77b51291990aa53e0a6fb968d3b941857cf61f
Temesgen dora <tdemeke36@gmail.com>
2024-02-12 09:02:07 -0800

    task: 9

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 const arr = process.argv;
-let temp = arr[2];
+let first = arr[2];
+let second = first;
+
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
   for (let i = 2; i < arr.length; i++) {
     for (let j = 2; j < arr.length; j++) {
-      if (temp < arr[i + 1]) {
-        temp = arr[i + 1];
+      if (first < arr[i + 1]) {
+        second = first;
+        first = arr[i + 1];
       }
     }
   }
-  console.log(temp);
+  console.log(second);
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 function add (a, b) {
-  const first = parseInt(a);
-  const second = parseInt(b);
-  if (isNaN(first) || isNaN(second)) console.log('NaN');
-  else if (first > 0) {
-    console.log(first + second);
-  }
+  console.log(a + b);
 }
-
-add(process.argv[2], process.argv[3]);
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));
