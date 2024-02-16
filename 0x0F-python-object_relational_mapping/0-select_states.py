a18096c673ebf41df08b3a86f26e6934d353241a
Temesgen dora <tdemeke36@gmail.com>
2024-02-16 06:59:38 -0800

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,12 +8,15 @@ username = arg[1]
 password = arg[2]
 dbname = arg[3]
 
-db = MySQLdb.connect(host="localhost", port = 3306, user=username, passwd=password, db=dbname, charset="utf8")
-cur = db.cursor()
-
-cur.execute("SELECT * FROM FROM hbtn_0e_0_usa.states ORDER BY states.id")
-
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port = 3306, user=username, passwd=password, db=dbname)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM FROM hbtn_0e_0_usa.states ORDER BY states.id")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()
 
 
 
