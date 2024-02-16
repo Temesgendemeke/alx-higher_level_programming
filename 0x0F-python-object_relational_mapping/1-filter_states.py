3545105e4fcc58c7a8f6202747f3a3178f55ba6a
Temesgen dora <tdemeke36@gmail.com>
2024-02-16 07:31:40 -0800

    done

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+import MySQLdb
+import sys
+
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'A%' ORDER BY states.id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()
