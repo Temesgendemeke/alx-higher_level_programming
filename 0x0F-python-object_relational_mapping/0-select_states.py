6904b3a0ed8f5c64427a5fa37a0acdf305b8598a
Temesgen dora <tdemeke36@gmail.com>
2024-02-16 07:22:52 -0800

    task 0 - fixs 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,14 @@
 import MySQLdb
 import sys
 
-arg = sys.argv
 # The code should not be executed when imported
 if __name__ == '__main__':
     # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port = 3306, user=arg[1], passwd=arg[2], db=arg[3])
+    db = MySQLdb.connect(host="localhost", port = 3306, user= sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for i in rows:
         print(i)
