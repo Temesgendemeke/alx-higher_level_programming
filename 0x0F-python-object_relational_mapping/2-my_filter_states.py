420d582f14ac9a912f5ea112540df0ad0abca4e6
Temesgen dora <tdemeke36@gmail.com>
2024-02-16 22:01:48 -0800

    done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC".format(argv[4])
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC;".format(argv[4]) 
     cur.execute(query)
     rows = cur.fetchall()
     for i in rows:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;"
+    query = "SELECT cities.id, cities.name, states.name  FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = '{}' ORDER BY cities.id ASC;".format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
+    j = []
     for i in rows:
-        print(i)
+        j.append(i[1])
+    print(", ".join(j))
     cur.close()
     db.close()
