7235236129ad630a1b709039450e5334a167ab33
Temesgen dora <tdemeke36@gmail.com>
2024-03-01 23:20:59 -0800

    tsk 0: fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 
 
 from urllib.request import urlopen
-with urlopen("https://alx-intranet.hbtn.io/status") as web:
-    body = web.read()
-    body_decoded = body.decode('utf-8')
-    print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body_decoded))
+if __name__ == '__main__':
+    with urlopen("https://alx-intranet.hbtn.io/status") as web:
+        body = web.read()
+        body_decoded = body.decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body_decoded))
