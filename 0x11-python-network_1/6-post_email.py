0ebf1a56bb96abc390d01d74fa4f24b75fded2f9
Temesgen dora <tdemeke36@gmail.com>
2024-03-05 05:00:14 -0800

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+from requests import post
+from sys import argv
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = {"email": argv[2]}
+    res = post(url, data=email)
+    print(res.text)
+    
