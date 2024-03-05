a28f16eacba737e7541ec4d684a2f149c3cb64e0
Temesgen dora <tdemeke36@gmail.com>
2024-03-05 10:58:47 -0800

    fixed: task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,8 +6,8 @@ from requests import get
 
 
 if __name__ == '__main__':
-    repo_name = argv[2]
-    owner = argv[1]
+    repo_name = argv[1]
+    owner = argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name)
     res = get(url)
 
