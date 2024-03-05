7a2c5db8bd6d73d3137112f1c5a017c948ca4104
tdemeke36@gmail.com <tdemeke36@gmail.com>
2024-03-05 09:25:51 -0800

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,15 +4,21 @@
 
 from requests import get, post
 from sys import argv
+import json
 
 if __name__ == '__main__':
-    q = argv[1]
-    if len(argv) == 1 or not q.isalpha():
-        print("No result")
-        q = ""
-    res = post("http://0.0.0.0:5000/search_user", data=q)
-    
-    print(res)
-    
-    
-    
+    letter = ""
+    if len(argv) == 1:
+        letter = ""
+    letter = argv[1]
+    payload = {"q": letter}
+    res = post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        response = res.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get('id'), response.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
