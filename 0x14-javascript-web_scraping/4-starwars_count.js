2c028d0086f224990ea21edc21428db24e1bf13f
temesgendemeke <tdemeke36@gmail.com>
2024-03-26 14:39:36 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+count = 0;  
+const obj = JSON.parse(body);
+  for (const key in obj.results) {
+    for (const i in (obj.results[key].characters)) {
+      if (obj.results[key].characters[i] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
