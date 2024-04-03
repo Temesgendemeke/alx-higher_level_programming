a2bbd1e209c5f85069d06eaf0c91db481e19e549
Temesgen dora <tdemeke36@gmail.com>
2024-04-03 14:19:24 -0700

    jquery project

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 $.get('https://swapi-api.alx-tools.com/api/films/?format=json', (data)=>{
     for (i in data["results"])
     {
-        $('UL#list_movies').append(data["results"][i].title);
+        $('UL#list_movies').append('<li>' + data["results"][i].title + '</li>');
     }
 });
